027075c2fbd3c787e8c1e263d1f971d8305b4740
Loisyy <nkeiruloisy@yahoo.com>
2023-09-11 18:09:26 +0100

    html structure updated

--- src/app.js
+++
@@ -1,7 +0,0 @@
-alert("say hi");
-
-let city = Abuja;
-
-let apiKey = "efa95fd2aa3co217t49d0b70baa77090";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-console.log(apiUrl);
